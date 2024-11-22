e68f728638b114b9e2baad83460b872d92b6e759
jualobcab <juanjo.lc2323@gmail.com>
2024-11-22 21:27:00 +0100

    Commit Desesperado

--- proyectoAnimesPeliculas/script.js
+++ proyectoAnimesPeliculas/script.js
@@ -106,16 +106,32 @@ class Biblioteca {
     }
 
     /// Filtraje
-    filtrarPorGeneros(generos,tipo){ // tipo debe ser pelicula o anime
+    filtrarPorGeneros(listaGeneros,tipo){ // tipo debe ser pelicula o anime
         let res = [];
+        let coincide = false
+
         this[`lista${tipo.charAt(0).toUpperCase()+tipo.toLowerCase().slice(1)}}s`]
             .forEach(element => {
-                if (element.some(e => generos.includes(e))){ 
-                    /////// NO SE SI LO DE ARRIBA FUNCIONA
+                coincide = false;
+                element.generos.forEach(genero => {
+                    if(listaGeneros.includes(genero)){
+                        coincide = true;
+                    }
+                });
+                if (coincide){
                     res.push(element);
                 }
         });
         return res;
+    }
+
+    clasificarPorPuntuacion(tipo){
+        return this[`lista${tipo.charAt(0).toUpperCase()+tipo.toLowerCase().slice(1)}}s`].sort((a,b) => b - a);
+    }
+
+    marcarFavorito(id, tipo){
+        this[`lista${tipo.charAt(0).toUpperCase()+tipo.toLowerCase().slice(1)}}s`][id].setFavorito();
+        // Hacer que cargue al localStorage la biblioteca, para que se guarde el cambio hecho
     }
 }
 
@@ -133,6 +149,7 @@ function Pelicula(id,titulo,generos,anyo,duracion,favorito,puntuacion){
 Pelicula.prototype.setFavorito = function(){
     this.favorito = !this.favorito;
 }
+Pelicula.prototype.obtenerTipo = function(){return "Pelicula"}
 function Anime(id,titulo,generos,anyo,episodios,favorito,puntuacion){
     this.id = id;
     this.titulo = titulo;
@@ -144,4 +161,168 @@ function Anime(id,titulo,generos,anyo,episodios,favorito,puntuacion){
 }
 Anime.prototype.setFavorito = function(){
     this.favorito = !this.favorito;
+}
+Anime.prototype.obtenerTipo = function(){return "Anime"}
+
+function guardarBibliotecaEnLocalStorage(biblioteca) {
+    
+    
+    // Guardamos el objeto en localStorage
+    localStorage.setItem("biblioteca", JSON.stringify(biblioteca));
+}
+
+function cargarBibliotecaDesdeLocalStorage() {
+    // Obtenemos el objeto de localStorage
+    const bibliotecaGuardada = localStorage.getItem("biblioteca");
+    
+    // Si existe el objeto en localStorage
+    if (bibliotecaGuardada) {
+        // Devolvemos el objeto Biblioteca
+        let res = JSON.parse(bibliotecaGuardada);
+        let bibliotecaAux = new Biblioteca();
+        bibliotecaAux.listaAnimes = res.listaAnimes;
+        bibliotecaAux.listaPeliculas = res.listaPeliculas;
+
+        return bibliotecaAux;
+    }
+    
+    // Si no existe en localStorage, devolvemos una nueva instancia vacía
+    return new Biblioteca();
+}
+
+function botonBorrado(id, tipo) {
+    if (tipo == "Anime"){
+        biblioteca.eliminarAnime(id);
+    }
+    else if (tipo == "Pelicula"){
+        biblioteca.eliminarPelicula(id);
+    }
+
+    guardarBibliotecaEnLocalStorage(biblioteca);
+    pintarLista(biblioteca[`lista${tipo.charAt(0).toUpperCase()+tipo.toLowerCase().slice(1)}}s`]);
+}
+
+function pintarLista(array) {
+    let res = "";
+
+    array = array.sort((a,b) => b.puntuacion - a.puntuacion);
+
+    array.forEach(instancia => {
+        res += "<div class='card' style='width: 18rem;'><div class='card-body'>";
+        res += "<div class='tituloBorrado'><h5 class='card-title'>"+instancia.titulo+"</h5>";
+        res += "<div class='icon-delete' onclick='botonBorrado("+instancia.id+",'"+instancia.obtenerTipo()+"')'></div></div>";
+        res += "<h6 class='card-subtitle mb-2 text-muted'>"+instancia.obtenerTipo()+"</h6>";
+        res += "<fieldset><legend>Géneros:</legend><ul>";
+        
+        instancia.generos.forEach(genero => {
+            res += "<li><p class='card-text'>"+genero+"</p></li>";
+        })
+        
+        res += "</ul></fieldset><p class='card-text'>·Año: "+instancia.anyo+"</p>";
+        if (instancia.obtenerTipo() == 'Anime'){
+            res += "<p class='card-text'>·Episodios: "+instancia.episodios+"</p>";
+        }
+        else {
+            res += "<p class='card-text'>· Duracion: "+instancia.duracion+"</p>";
+        }
+        res += "<div class='puntuacionYFavorito'><p class='card-text puntuacion'>"+instancia.puntuacion+"</p>";
+        res += "<label class='like'><input class='corazon' type='checkbox' onclick='biblioteca.marcarFavorito("+instancia.id+", "+instancia.obtenerTipo()+")'";
+        if (instancia.favorito){
+            res += "checked='true'";
+        }
+
+        res += "/><div class='hearth'></div></label></div></div></div>";
+    });
+
+    document.getElementById("listado").innerHTML = res;
+}
+
+function listarGeneros(tipo){
+    let res = "";
+    let arrayAux;
+
+    if (tipo == "Anime"){
+        arrayAux = generosAnime;
+    }
+    else {
+        arrayAux = generosPeliculas;
+    }
+    arrayAux.forEach(genero => {
+        res += "<option value='"+genero+"'>"+genero+"</option>"
+    });
+
+    document.getElementById("generos").innerHTML = res;
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById('botonFiltrar').addEventListener("click", (event) => {
+        event.preventDefault();
+        let tipo = formulario.tipo.value;
+        let genero = formulario.generos.value;
+
+        pintarLista(biblioteca.filtrarPorGeneros([genero],tipo).clasificarPorPuntuacion(tipo));
+    });
+    listarGeneros("Pelicula");
+
+    document.getElementById('menuPelicula').addEventListener("click", (event) => {
+        pintarLista(biblioteca['listaPeliculas']);
+    })
+    document.getElementById('menuAnime').addEventListener("click", (event) => {
+        pintarLista(biblioteca['listaAnimes']);
+    })
+});
+
+////////////////////////////////////////////////////////
+//////////////      MAIN      //////////////////////////
+////////////////////////////////////////////////////////
+let biblioteca = cargarBibliotecaDesdeLocalStorage();
+
+
+/* Ejecutar para cargar algunas instancias */
+function pruebaProfesor(){
+    let peli1 = new Pelicula(1,"Dune", ["Acción","Guerra","Aventura"], "2022", "200 mnts", true, 9.5);
+    let peli2 = new Pelicula(2,"Toy Story",["Aventura","Animación"],"1995","122 mnts",false,7);
+    let peli3 = new Pelicula(3,"Alien",["Terror psicológico","Thriller","Acción"],"1978","117 mnts",false,8);
+    let peli4 = new Pelicula(4,"ET",["Aventura","Romance"],"1980","158 mnts",false,6);
+    let peli5 = new Pelicula(5,"Viernes 13",["Horror","Misterio","Suspense"],"1999","120 mnts",false,7);
+    let peli6 = new Pelicula(6,"Starship Troopers",["Ciencia ficción (Sci-Fi)","Comedia","Acción"],"2000","2 mnts",true,8.5);
+    let peli7 = new Pelicula(7,"Terminator",["Acción","Aventura", "Aventura"],"1984","147 mnts",false,7);
+    let peli8 = new Pelicula(8,"Lilo & Stitch",["Animación","Comedia"],"2008","120 mnts",true,8.75);
+    let peli9 = new Pelicula(9,"Frozen",["Animación","Musical","Comedia"],"2020","160 mnts",true,9.9);
+    let peli10 = new Pelicula(10,"Torrente 2",["Comedia","Crimen"],"2000","133 mnts",true,8);
+
+    let anime1 = new Anime(1,"One Piece",["Acción","Aventura","Comedia","Shonen"],"2003","1100 caps",true,9);
+    let anime2 = new Anime(2,"School Days",["Terror","Gore","Psicológico"],"2014","24 caps",false,6);
+    let anime3 = new Anime(3,"Prision School",["Comedia","Drama"],"2018","24 caps",true,9.5);
+    let anime4 = new Anime(4,"Gran Blue",["Comedia"],"2017","24 caps",true,9.4);
+    let anime5 = new Anime(5,"Bobobo",["Comedia","Acción","Shonen"],"1995","200 caps",false,7.6);
+    let anime6 = new Anime(6,"Keroro",["Comedia"],"1998","300 caps",false,7);
+    let anime7 = new Anime(7,"Shin-chan",["Comedia", "Slice of Life"],"1999","600 caps",true,8.8);
+    let anime8 = new Anime(8,"Doraemon",["Drama","Aventura","Comedia"],"2001","100 caps",false,7);
+    let anime9 = new Anime(9,"Black Clover",["Acción","Aventura","Shonen"],"2019","200 caps",true,9);
+    let anime10 = new Anime(10,"Naruto",["Acción","Aventura","Shonen"],"2009","900 caps",true,9);
+
+    biblioteca = new Biblioteca();
+    biblioteca.agregarPelicula(peli1);
+    biblioteca.agregarPelicula(peli2);
+    biblioteca.agregarPelicula(peli3);
+    biblioteca.agregarPelicula(peli4);
+    biblioteca.agregarPelicula(peli5);
+    biblioteca.agregarPelicula(peli6);
+    biblioteca.agregarPelicula(peli7);
+    biblioteca.agregarPelicula(peli8);
+    biblioteca.agregarPelicula(peli9);
+    biblioteca.agregarPelicula(peli10);
+
+    biblioteca.agregarAnime(anime1);
+    biblioteca.agregarAnime(anime2);
+    biblioteca.agregarAnime(anime3);
+    biblioteca.agregarAnime(anime4);
+    biblioteca.agregarAnime(anime5);
+    biblioteca.agregarAnime(anime6);
+    biblioteca.agregarAnime(anime7);
+    biblioteca.agregarAnime(anime8);
+    biblioteca.agregarAnime(anime9);
+    biblioteca.agregarAnime(anime10);
+    guardarBibliotecaEnLocalStorage(biblioteca);
 }
