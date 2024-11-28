5a5cd2f90bc8561d7efc7bd9f7b51c440835e0ab
jualobcab <juanjo.lc2323@gmail.com>
2024-11-28 14:26:16 +0100

    Commit trabajo cliente

--- proyectoCoches/script.js
+++ proyectoCoches/script.js
@@ -4,7 +4,7 @@
 //////////       FUNTIONS      /////////////////////////
 ////////////////////////////////////////////////////////
 ////////////////////  Variables   //////////////////////
-
+let listaVehiculos=[];
 
 ////////////////////  Constantes  //////////////////////
 const Combustible = Object.freeze({
@@ -13,6 +13,13 @@ const Combustible = Object.freeze({
     Electrico:5,
     Gas:5
 });
+const TipoMoto = [
+    "Deportiva",
+    "Custom",
+    "Motocross",
+    "Adventure",
+    "Scooter"
+]
 
 //////////////////////  Clases  /////////////////////////
 class Vehiculo {
@@ -23,14 +30,23 @@ class Vehiculo {
         this.precio = precio;
         this.stock = stock;
     }
+    vender() {
+        this.stock = !this.stock;
+    }
+    obtenerDescripcion(){
+        res = "Marca: "+this.marca+", Modelo: "+this.modelo+", Año: "+this.anyo+", Precio: "+this.precio+", Stock: ";
+        if (this.stock){
+            res += "Disponible"
+        }
+        else {
+            res += "No disponible"
+        }
+        return res
+    }
 }
 class Coche extends Vehiculo {
     constructor (marca,modelo,anyo,precio,stock,numeroPuertas,matricula,tipoCombustible){
-        super(marca);
-        super(modelo);
-        super(anyo);
-        super(precio);
-        super(stock);
+        super(marca,modelo,anyo,precio,stock);
         this.numeroPuertas = numeroPuertas;
         this.matricula = matricula;
         this.tipoCombustible = tipoCombustible;
@@ -40,14 +56,21 @@ class Coche extends Vehiculo {
     calcularImpuestos(){
         return this.precio*(Combustible[this.tipoCombustible]/100)
     }
+    obtenerDescripcion(){
+        res = "Marca: "+this.marca+", Modelo: "+this.modelo+", Año: "+this.anyo+", Precio: "+this.precio+", Stock: ";
+        if (this.stock){
+            res += "Disponible"
+        }
+        else {
+            res += "No disponible"
+        }
+        res += "Nº Puertas: "+this.numeroPuertas+", Matricula: "+this.matricula+", Combustible: "+this.tipoCombustible;
+        return res
+    }
 }
 class Motocicleta extends Vehiculo {
     constructor (marca,modelo,anyo,precio,stock,matricula,cilindrada,tipo){
-        super(marca);
-        super(modelo);
-        super(anyo);
-        super(precio);
-        super(stock);
+        super(marca,modelo,anyo,precio,stock);
         this.matricula = matricula;
         this.cilindrada = cilindrada
         this.tipo = tipo;
@@ -62,4 +85,24 @@ class Motocicleta extends Vehiculo {
             return this.precio*15/100;
         }
     }
+    obtenerDescripcion(){
+        res = "Marca: "+this.marca+", Modelo: "+this.modelo+", Año: "+this.anyo+", Precio: "+this.precio+", Stock: ";
+        if (this.stock){
+            res += "Disponible"
+        }
+        else {
+            res += "No disponible"
+        }
+        res += "Matricula: "+this.matricula+", Cilindrada: "+this.cilindrada+", Tipo: "+this.tipo;
+        return res
+    }
+}
+
+////////////////      FUNCIONES     ///////////////////
+function crearCoche(marca,modelo,anyo,precio,stock,numeroPuertas,matricula,tipoCombustible){
+    let resCoche = new Coche(marca,modelo,anyo,precio,stock,numeroPuertas,matricula,tipoCombustible);
+
+}
+function crearMotocicleta(marca,modelo,anyo,precio,stock,matricula,cilindrada,tipo) {
+
 }
